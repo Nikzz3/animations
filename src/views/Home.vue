94558b19c8667a432738fd8971e48ba06e5df2bf
Nick Zimmermann <nick.zimmermann@swisscom.com>
2022-02-25 11:04:47 +0100

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-icon-heart-fill class="heart-beating h1" />
+    </b-row>
+    <b-row>
+      <b-icon-heart-fill class="heart-moving h1" />
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import { BIconHeartFill } from 'bootstrap-vue';
+
+export default {
+  name: 'Home',
+  components: {
+    BIconHeartFill
+  }
+};
+</script>
+<style scoped>
+.heart-beating {
+  animation: beating 500ms infinite;
+}
+
+@keyframes beating {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+.heart-moving {
+  animation: moving 500ms infinite;
+}
+
+@keyframes moving {
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(50%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+</style>
